5acb0b7d5b9576ed08b90a6e77f29bbc892952f9
Jeong-Rae <kkwjdfo@gmail.com>
2024-05-07 03:41:30 +0900

    [Test] lookup_test case  추가

--- test/lookup_test.js
+++ test/lookup_test.js
@@ -11,6 +11,24 @@ export const mocks = [
             },
         },
     ],
+    [
+        {
+            $lookup: {
+                from: "members",
+                localField: "enrollmentlist",
+                foreignField: "name",
+                as: "enrollee_info",
+            },
+        },
+    ],
+    [{
+        $lookup: {
+           from: "items",
+           localField: "item",    // field in the orders collection
+           foreignField: "item",  // field in the items collection
+           as: "fromItems"
+        }
+     }]
 ];
 
 for (const mock of mocks) {
